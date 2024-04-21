74c11ea07ef538e3b0427160a91c596768065e2a
Kamugisha Bruce <brucekamugisha36@gmail.com>
2024-04-21 11:40:46 +0300

    configured allowed hosts

--- venv/muggedhead/muggedhead/settings.py
+++ venv/muggedhead/muggedhead/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mt&jwqh(d-$i7e0wg2$bb12j-m%&5)t^dq4r7h$*vi(2ngn$ic
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['mugged-head-bacc052ddf71.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
